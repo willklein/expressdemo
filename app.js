686b97d194f83530776f09defe2f6423d411eac7
Will Klein <will.klein@gmail.com>
2013-05-25 20:06:54 -0400

    Tidy up the code a little bit for sharing the demo app.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// basic HTTP server example
+
 //var http = require('http');
 //
 //http.createServer(function(req, res) {    
@@ -6,18 +8,16 @@
 
 var express = require('express');
 
-var events = require('/models/events')
-
 var app = express();
 
 var events = [];
 
+// mock out an event
 events.push({
     title: 'Hartford Code Camp 6',
     location: 'Bloomfield, CT',
     time: '9am - 5pm'
 });
-
 
 app.set('port', 8000);
 app.set('views', __dirname + '/views');
@@ -25,13 +25,17 @@ app.set('view engine', 'jade');
 
 app.use(express.bodyParser());
 
-
+// demonstrate basic Jade template
 app.get('/', function(req, res) {
-//    res.end('hello with express');
     res.render('index', {
         title: 'Home',
         message: 'hello javascript'
     });
+});
+
+// demonstrate a very simple route w/ response
+app.get('/bye', function(req, res) {
+    res.send('bye');
 });
 
 app.get('/events', function(req, res) {
@@ -41,7 +45,7 @@ app.get('/events', function(req, res) {
     }); 
 });
 
-app.get('/event/new', function(req, res) {
+app.get('/new', function(req, res) {
     res.render('new', {
         title: 'New Event'
     });
@@ -57,10 +61,6 @@ app.post('/new', function(req, res) {
     events.push(newEvent);
     
     res.redirect('/events');
-});
-
-app.get('/bye', function(req, res) {
-    res.send('bye'); 
 });
 
 app.listen(app.get('port'));
